449c617bb8ffdf778fc97b6d834e2ec7ac7903b7
Александр Бурыгин <8833106@gmail.com>
2020-04-01 22:07:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var a = 5;
+
+let incr = 10,
+    decr = 10;
+
+   
+
+    console.log(incr);
+    console.log(decr);
+
+    console.log('2'== 2);
+
+    let isChecked = true,
+        isClose = true;
+
+        console.log(isChecked || isClose);
