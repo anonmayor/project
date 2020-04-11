bfe2799a54519a0e3eee04031245dbce23b750ff
Aleksandr <8833106@gmail.com>
2020-04-11 12:42:48 +0300

    11.04.2020

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 'use strict';
 
-var a = 5;
+let money = prompt('Ваш бюджет на месяц');
+let time = prompt('Введите дату в формате YYYY-MM-DD');
 
-let incr = 10,
-    decr = 10;
 
-   
 
-    console.log(incr);
-    console.log(decr);
+let appData = {
 
-    console.log('2'== 2);
+    budjet: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
 
-    let isChecked = true,
-        isClose = true;
+};
 
-        console.log(isChecked || isClose);
+
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
+
+alert(appData.budjet / 30);
